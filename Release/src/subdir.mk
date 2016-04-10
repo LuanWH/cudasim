bf8395b238f4612de4b1026e2a759baafd7a71a4
LuanWH <lwhluan@gmail.com>
2016-04-11 00:06:26 +0800

    Revert "update gitignore"
    This reverts commit 9c787629aa8937ad713906c626bef488241002fc.

---
+++ Release/src/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../src/main.cu 
+
+CU_DEPS += \
+./src/main.d 
+
+OBJS += \
+./src/main.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-7.0/bin/nvcc -O3 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_52,code=sm_52  -odir "src" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-7.0/bin/nvcc -O3 --compile --relocatable-device-code=false -gencode arch=compute_35,code=compute_35 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_52,code=sm_52  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
